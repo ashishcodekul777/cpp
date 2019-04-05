eca61867db1757cd9ebc8f61665a19cd4679587f
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-05 16:26:14 +0530

    commit "cpp demo"

--- 4th-april-2019/academy.cpp
+++ 4th-april-2019/academy.cpp
@@ -1,8 +1,8 @@
-#include<iostream.h>
+#include<iostream.h
 using namespace std;
 int main()
 {
-    char grade='A;
+    char grade='A';
     string phrase="Giraffe Academy";
     int age=20;
     double gpa=4.2;
