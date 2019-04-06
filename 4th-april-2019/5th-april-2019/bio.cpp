e97a25261c715c62aefe15926caaaa142d3fa238
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-06 14:35:58 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/bio.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"\n Enter your age=";
+    cin>>age;
+    cout<<"\n Your age="<<age<<"years";
+    return 0;
+}
