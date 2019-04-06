61c15d5cdeb32ba10ebbeb381f3907ffbf78abf9
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-06 15:29:27 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/getline.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"\n Enter your name=";
+    getline(cin,name);
+    cout<<"\n Hello   "<<name;
+    return 0;
+}
