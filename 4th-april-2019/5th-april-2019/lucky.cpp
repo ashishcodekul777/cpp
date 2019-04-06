435e9089990cc7fc6ff3f6c675cda2ed7ba2db6b
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-06 17:51:49 +0530

    commit cpp demos

--- 4th-april-2019/5th-april-2019/lucky.cpp
+++ 4th-april-2019/5th-april-2019/lucky.cpp
@@ -1,8 +1,8 @@
-#include<iostrem>
+#include<iostream>
 using namespace std;
 int main()
 {
    int luckyNums[20]={24,15,27,32,65,54};
-   luckynums[10]=100;
-   cout<<"\n Lucky numbers  "<<luckynums[10];
+   luckyNums[10]=100;
+   cout<<"\n Lucky numbers  "<<luckyNums[10];
 }
