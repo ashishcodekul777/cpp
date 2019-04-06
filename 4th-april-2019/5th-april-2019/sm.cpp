3a6a34ee10802652016eee45e48cec30d237429b
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-06 16:08:14 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/sm.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double num1,num2,sum;
+    cout<<"\n Enter the 2 nos=";
+    cin>>num1>>num2;
+    cout<<"\n The Sum=  "<<sum;
+    return 0;
+}
