362932599276dacb31cc5be0515aef6554d6cb41
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-06 15:46:27 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"\n Enter your first nos=";
+    cin>>num1;
+    cout<<"\n Enter your second nos=";
+    cin>>num2;
+    cout<<"\n Sum=  "<<num1+num2;
+    return 0;
+}
