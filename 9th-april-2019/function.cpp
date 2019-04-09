984596762b47b98c797a68fa1b395be3160f3933
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-09 10:49:10 +0530

    commit cpp demos

---
+++ 9th-april-2019/function.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void sayHi(string name,int  age)
+{
+     cout<<"Hello "<<name<<"You are "<<age<<"Years ";
+}
+int main()
+{
+    sayHi("Mike",60);
+    return 0;
+}
