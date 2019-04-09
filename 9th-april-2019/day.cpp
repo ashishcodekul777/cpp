e09c60ece86a6f45f15763a93d745e234b7b057a
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-09 16:59:52 +0530

    commit cpp demos

---
+++ 9th-april-2019/day.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+    string DayofWeek(int dayNum)
+    {
+      string dayName;
+       switch(dayNum)
+       {
+        case 1:
+          dayName="Sunday";
+        break;
+        case 2:
+          dayName="Monday";
+        break;
+        case 3:
+         dayName="Tuesday";
+        break;
+        case 4:
+         dayName="Wednesday";
+        break;
+        case 5:
+         dayName="Thursday";
+        break;
+        case 6:
+         dayName="Friday";
+        break;
+        case 7:
+         dayName="Saturday";
+        default:
+         cout<<"\n Invalid User";
+       }
+      return dayName;
+     }
+    int main()
+    {
+        DayofWeek(7);
+        return 0;
+    }   
+
+
+
