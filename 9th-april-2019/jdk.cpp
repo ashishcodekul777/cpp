e4a524a46398a1570f38f26d11cd254f5eb68202
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-09 15:16:34 +0530

    commit cpp demos

---
+++ 9th-april-2019/jdk.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int getmax(int num1,int num2,int num3)
+{
+    int result;
+    if(num1>=num2 && num1>=num3)
+     result=num1;
+    else if(num2>=num3 && num2>=num1)
+     result=num2;
+    else
+     result=num3;
+    return result;
+}
+int main()
+{
+    cout<<getmax(2, 5, 10);
+    return 0;
+}       
