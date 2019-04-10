3d9cd1bae0ee6f77c4400d19261e975bc24624a7
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-10 10:38:58 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/10th-april-2019/index.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int index;
+    index=1;
+    while(index<=10)
+    {
+        cout<<"\n Index"<<endl;
+        index++;
+    }
+    return 0;
+}
