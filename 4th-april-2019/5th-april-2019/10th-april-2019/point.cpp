84216fa5e191d0eab1fc0369657477c39fed4912
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-10 16:34:44 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/10th-april-2019/point.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int age=18;
+    double gpa=2.8;
+    string name="Mike";
+    cout<<"\n Age= "<<&age<<endl;
+    cout<<"\n GPA= "<<&gpa<<endl;
+    cout<<"\n Name= "<<&name<<endl;
+    return 0;
+}
+   
