c90e922e9df6e15a59f759db6da3e08b9c6509a5
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-10 15:28:32 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/10th-april-2019/2D.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+   int numberGrid[3][2]={  {2,5},
+                           {3,2},
+                           {1,6}
+                        };
+   cout<<numberGrid[0][2];
+  return 0;
+}
