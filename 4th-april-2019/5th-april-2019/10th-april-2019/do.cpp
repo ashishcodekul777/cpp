ca10a6277157f21dcd9fb37e5b4c6b6b1004e5d9
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-10 10:53:12 +0530

    commit cpp demos

---
+++ 4th-april-2019/5th-april-2019/10th-april-2019/do.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int i=6;
+    do
+    {
+        cout<<"\n Index"<<endl;
+        i++;
+    }while(i<=5);
+  return 0;
+}
