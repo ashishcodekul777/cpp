be1fae8b35eed016c476efe814d8c577ed0fc397
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-11 10:47:53 +0530

    commit cpp demos

---
+++ 11th-april-2019/class.c
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class student
+{
+    public:
+    int rollno;
+    string name;
+    float per;
+};
+int main()
+{
+    student S1;
+    S1.rollno=101;
+    S1.name="Ashish";
+    S1.per=80.02;
+    cout<<"\n Rollno= "<<S1.rollno;
+    cout<<"\n Name=  "<<S1.name;
+    cout<<"\n Perentage=  "<<S1.per;
+    return 0;
+}
