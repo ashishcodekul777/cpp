334b29537c1e6f04b8de6a2b910cd02ffe945a10
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-12 18:19:47 +0530

    commit c demos

---
+++ 11th-april-2019/12th-april-2019/sam.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int c,n;
+    printf("\n Random numbers in 1 to 100");
+    for(c=1;c<=10;c++)
+    {
+        n=rmd() % 100 +1;
+        printf("\n %d  ",n);
+    }
+    return 0;
+}
