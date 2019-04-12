50928a8fab4325dd9d055650558a9a882e6c4621
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-12 12:09:35 +0530

    gcc cpp demos

---
+++ 4th-april-2019/5th-april-2019/exercise/fruit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char apple;
+    char bananna;
+    char grapes;
+    int c=0;
+    printf("\n Enter the nos of fruits=");
+    scanf("%c%c%c"&apple,&banana,&grapes);
+    printf("nos of fruits=%d ",c);
+    return 0;
+
+}
