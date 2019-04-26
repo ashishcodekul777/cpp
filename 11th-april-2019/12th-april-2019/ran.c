edb87f4af76cb1685876e31cdb0af9e8d1d064e7
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-26 10:04:09 +0530

    commit cpp demos

---
+++ 11th-april-2019/12th-april-2019/ran.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int inputFuction();
+int getRandomnos();
+void ckeckRandomnos()
+{
+    int nos,rnos;
+    nos=0,rnos=0;
+    nos=inputFunction();
+    rnos=getRandomnos();
+    checkRandomnos(int nos,int rnos);
+    return no;
+}
+int inputFunction()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        nos=rand()%100+1;
+        printf("\n the  ramdom nos=%d",)
+    }
+}   
+
+  
