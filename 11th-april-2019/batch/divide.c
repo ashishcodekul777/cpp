6cb834c2f2a6a6fb6c1680ef729f90e31f1ced71
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-27 11:09:21 +0530

    commit c demos

--- 11th-april-2019/batch/divide.c
+++ 11th-april-2019/batch/divide.c
@@ -5,7 +5,7 @@ int main()
     int nos;
     printf("\n Enter the number=");
     scanf("%d",&nos);
-    if(nos%5 && nos%11)
+    if(nos%5==0 && nos%11==0)
      printf("The given nos is divisible by 5");
     else
      printf("\n The given nos is divisible by 11");
