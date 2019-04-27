4a120111bf98753ceca42f355abbe21c93a3298c
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-27 11:55:48 +0530

    commit c demos

---
+++ 11th-april-2019/batch/reverse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\n Enter the nos=");
+    scanf("%d",&n);
+    while(n>=1)
+    {
+      printf("\n %d ",n );
+      n--;
+    }
+    return 0;
+}
