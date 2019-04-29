5d2f693a8397bd55111a8a31270e91af428a4b35
ashishcodekul777 <ashish444rao@gmail.com>
2019-04-29 15:36:04 +0530

    commit c demos

--- 11th-april-2019/batch/unique.c
+++ 11th-april-2019/batch/unique.c
@@ -13,26 +13,28 @@ int main()
        scanf("%d",&n);
        printf("Input %d elements in the array :\n",n);
        for(i=0;i<n;i++)
-            {
+       {
 	      printf("element - %d : ",i);
 	      scanf("%d",&arr1[i]);
-	    }
+	   }
     printf("\nThe unique elements found in the array are : \n");
     for(i=0; i<n; i++)
     {
-        ctr=0;  for(j=0; j<i-1; j++)
+        for(j=0; j<i-1; j++)
         {
             if(arr1[i]==arr1[j])
             {
                 ctr++;
             }
-        }for(k=i+1; k<n; k++)
+        }
+        for(k=i+1; k<n; k++)
         {
             if(arr1[i]==arr1[k])
             {
                 ctr++;
             }
-        }if(ctr==0)
+        }
+        if(ctr==0)
         {
           printf("%d ",arr1[i]);
         }
