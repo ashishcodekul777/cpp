1caa320533c9ab5170c063443c32ff106ab540db
ashishcodekul777 <ashish444rao@gmail.com>
2019-05-03 11:25:54 +0530

    commit c demos

--- 11th-april-2019/batch/evi.c
+++ 11th-april-2019/batch/evi.c
@@ -1,39 +1,36 @@
-#include <stdio.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 void main()
  {
     int arr1[10], arr2[10], arr3[10];
     int i,j=0,k=0,n;
 	
 	printf("\n\nSeparate odd and even integers in separate arrays:\n");
-       printf("------------------------------------------------------\n");	
+    printf("------------------------------------------------------\n");	
 
        printf("Input the number of elements to be stored in the array :");
        scanf("%d",&n);
    
        printf("Input %d elements in the array :\n",n);
        for(i=0;i<n;i++)
-            {
+       {
 	      printf("element - %d : ",i);
-
-
- scanf("%d",&arr1[i]);
+          scanf("%d",&arr1[i]);
 	    }
 
     for(i=0;i<n;i++)
     {
-	if (arr1[i]%2 == 0)
-	{
-	   arr2[j] = arr1[i];
-	   j++;
-       }
-	else
-	{
-	   arr3[k] = arr1[i];
-	   k++;
-	}
+	  if (arr1[i]%2 == 0)
+	  {
+	     arr2[j] = arr1[i];
+	     j++;
+      }
+	  else
+	 {
+	    arr3[k] = arr1[i];
+	    k++;
+	 }
     }
-
     printf("\nThe Even elements are : \n");
     for(i=0;i<j;i++)
     {
@@ -43,9 +40,10 @@ void main()
     printf("\nThe Odd elements are :\n");
     for(i=0;i<k;i++)
     {
-	printf("%d ", arr3[i]);
+	  printf("%d ", arr3[i]);
     }
     printf("\n\n");	
  }
+ 
 
 
