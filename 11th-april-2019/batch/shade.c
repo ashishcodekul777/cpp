991f0980c1ff5c582a4b091809299a18598fe360
ashishcodekul777 <ashish444rao@gmail.com>
2019-05-10 16:57:00 +0530

    commit c demos

---
+++ 11th-april-2019/batch/shade.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+
+int  * getNEvenNumbers(const int N, int * numbers);
+
+
+int main()
+{
+    int evenNumbers[SIZE];
+    int i;
+    getNEvenNumbers(SIZE, evenNumbers);
+    printf("First %d even numbers are: \n", SIZE);
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d ", *(evenNumbers + i));
+    }
+    return 0;
+}
+int * getNEvenNumbers(const int N, int * numbers)
+{
+    int i;
+    for (i = 0; i < N; i++)
+    {
+        *(numbers + i) = 2 * (i + 1);
+    }
+
+    return numbers;
+}
