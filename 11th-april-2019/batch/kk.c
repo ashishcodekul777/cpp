ff2443f9d784213e9f19a2e90a0a811bdfa6fcc5
ashishcodekul777 <ashish444rao@gmail.com>
2019-05-10 15:28:28 +0530

    commit c demos

---
+++ 11th-april-2019/batch/kk.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+
+    int a = 10;
+    int b = 20;
+
+    printf("Before swap\na: %d\nb: %d\n",a ,b);
+    swap(&a, &b);
+    printf("After swap\na: %d\nb: %d\n",a ,b);
+    return 0;
+}
